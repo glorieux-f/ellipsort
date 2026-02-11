996fea176e954d8d5462759dfbd6da832c0fca03
Frédéric Glorieux <frederic.glorieux@fictif.org>
2026-02-11 20:59:02 +0100

    Horizontal card

---
+++ words.js
@@ -0,0 +1,11 @@
+const words = [
+  "il",
+  // Adverbes
+  "avant", "après", "beaucoup", "loin", "près", "puis",
+  // Adjectifs
+  "beau", "bleu", "calme", "clair", "content", "creux", "dangereux", "dur", "fatigué", "grand", "gris", "humide", "important", "jeune", "loin", "lourd", "malade", "mort", "mouillé", "neuf", "propre", "rouge", "sale", "sec", "seul", "tranquille", "utile", "vert", "vieux", "vivant", 
+  // ponctuation
+  " … ", ".", ",", ":", "?", "!", "(", ")", "«", "»",
+   "—", "+", "/", "×", "=", "≠", "<", ">", "∃", "⇒", "⇔", "⇐", "∑", "%", "↑", "↓", "←", "→", "↔", "de", "à", "le", "la", "el", "un", "une", "des", "je", "tu", "on", "iel", "il", "elle", "vous", "nous", "eux", "être", "avoir", "faire", 
+
+]
