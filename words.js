e3b2377c95f3a9ce8c2a214c5039898073203b39
Frédéric Glorieux <frederic.glorieux@fictif.org>
2026-02-12 00:31:36 +0100

    multiple fonts

--- words.js
+++ words.js
@@ -1,11 +1,25 @@
 const words = [
-  "il",
+  ".", "(", "…", "à", "?", "g",// 1
+  "il", "je", // 2
+  "ici", "pur",  // 3
+  "vert", "fort",  // 4
+  "rouge",  "sale",// 5
+  "propre", // 6
+  "mouillé", // 7
+  // 8
+  "important", // 9
+  "tranquille", // 10
+];
+
+const words2 = [
   // Adverbes
-  "avant", "après", "beaucoup", "loin", "près", "puis",
+  "avant", "après", "beaucoup", "ici", "là", "loin", "près", "puis",
   // Adjectifs
-  "beau", "bleu", "calme", "clair", "content", "creux", "dangereux", "dur", "fatigué", "grand", "gris", "humide", "important", "jeune", "loin", "lourd", "malade", "mort", "mouillé", "neuf", "propre", "rouge", "sale", "sec", "seul", "tranquille", "utile", "vert", "vieux", "vivant", 
+  "bas", "beau", "bleu", "bon", "calme", "carré", "chaud", "clair", "content", "court", "creux", "doux", "dangereux", "dur", "électrique", "fatigué", "fort", "fou", "froid", "grand", "gris", "haut", "humide", "important", "jeune", "loin", "long", "lourd", "malade", "mauvais", "méchant", "mort", "mouillé", "neuf", "noir", "propre", "rouge", "sale", "sec", "seul", "sombre", "tranquille", "utile", "vert", "vieux", "vivant", 
+  // déterminants
+  "la", "le", "les", "el", "un", "une", "des", 
   // ponctuation
   " … ", ".", ",", ":", "?", "!", "(", ")", "«", "»",
-   "—", "+", "/", "×", "=", "≠", "<", ">", "∃", "⇒", "⇔", "⇐", "∑", "%", "↑", "↓", "←", "→", "↔", "de", "à", "le", "la", "el", "un", "une", "des", "je", "tu", "on", "iel", "il", "elle", "vous", "nous", "eux", "être", "avoir", "faire", 
+   "—", "+", "/", "×", "=", "≠", "<", ">", "∃", "⇒", "⇔", "⇐", "∑", "%", "↑", "↓", "←", "→", "↔", "de", "à", "je", "tu", "on", "iel", "il", "elle", "vous", "nous", "eux", "être", "avoir", "faire", 
 
 ]
