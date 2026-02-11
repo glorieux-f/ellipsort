d578325857ff2ebb78e97ce11150085d8e0165f7
Frédéric Glorieux <frederic.glorieux@fictif.org>
2026-02-11 13:03:40 +0100

    vertical version, not OK

---
+++ ellipsort.js
@@ -0,0 +1,10 @@
+const ellipsort = [
+  // ponctuation
+  "…", ".", ",", ":", "?", "!", "(", ")", "«", "»",
+   "—", "+", "/", "×", "=", "≠", "<", ">", "∃", "⇒", "⇔", "⇐", "∑", "%", "↑", "↓", "←", "→", "↔", "de", "à", "le", "la", "el", "un", "une", "des", "je", "tu", "on", "iel", "il", "elle", "vous", "nous", "eux", "être", "avoir", "faire", 
+  // Adverbes
+  "avant", "après", "beaucoup", "loin", "près", "puis",
+  // Adjectifs
+  "beau", "bleu", "calme", "clair", "content", "creux", "beau", "bleu", "dangereux", "dur", "fatigué", "grand", "gris", "humide", "imortant", "jeune", "loin", "lourd", "malade", "mort", "mouillé", "neuf", "propre", "rouge", "sale", "sec", "seul", "tranquille", "utile", "vert", "vieux", "vivant", 
+
+]
